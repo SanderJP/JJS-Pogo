75ea0e939f9ed7cad3afacb085c28b174693884a
Jordi <git@jordimenheer.nl>
2012-11-11 14:59:09 +0100

    Added a 1 button menu to flip screen orientation
    If using a phone, use this button/menu to change the screen orientation
    to portrait.

--- src/nl/hr/minor/jjs/pogo/GamePogo.java
+++ src/nl/hr/minor/jjs/pogo/GamePogo.java
@@ -1,12 +1,25 @@
 package nl.hr.minor.jjs.pogo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.GestureDetector.OnDoubleTapListener;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
+import android.view.View.OnClickListener;
 
 public class GamePogo extends Activity {
 
 	private GameView _gv;
 	private boolean _gamePaused = false;
+	private String _activityOrientation = "landscape";
 	
 	/** Called when the activity is first created. */
     @Override
@@ -19,6 +32,7 @@ public class GamePogo extends Activity {
         
         _gv = new GameView(this);
         setContentView(_gv);
+        
     }
 
 	@Override
@@ -51,4 +65,34 @@ public class GamePogo extends Activity {
 		}
 		
 	}
+	
+	private void flipOrientation(){
+		if(_activityOrientation == "portrait"){
+			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+			_activityOrientation = "landscape";
+		} else {
+			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+			_activityOrientation = "portrait";
+		}
+	}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.gameoptions, menu);
+    	return true;
+    } 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+		    case R.id.flipOrientation:
+		    	this.flipOrientation();
+		    return true;
+	    }
+	    return false;
+    }
+	
+	
 }
